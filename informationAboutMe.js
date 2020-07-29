39d213e3f3848354ed79a0f553ce27416e7a519f
влад семенков <semenkovvlad2000@gmail.com>
2020-07-29 13:10:55 +0000

    add some information about me

---
+++ informationAboutMe.js
@@ -0,0 +1,8 @@
+const informationAboutMe = {
+  name: 'Vlad',
+  surname: 'Semenkov',
+  university: 'BSU Mechanics and Mathematics Faculty',
+  location: 'Minsk',
+  age: '20',
+  hobby: 'football and voleyball'
+};
