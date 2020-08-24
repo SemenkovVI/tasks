6b2d52dc3d0a93d81efe43d9852ae487276beb35
Vlad Semenkov <56255056+SemenkovVI@users.noreply.github.com>
2020-08-25 01:41:53 +0300

    fixed codestyle moments

--- task3AndersenPromise.js
+++ task3AndersenPromise.js
@@ -1,5 +1,5 @@
-const User = {};
-const ID = {
+const USER_OBJECT = {};
+const OBJECT_OF_ID = {
   1: 7,
   2: 5,
   3: 3,
@@ -8,32 +8,32 @@ const ID = {
 const promise1 = new Promise((resolve) => {
   setTimeout(() => resolve('Minsk'), 1000); 
 }).then((result) => {
-  User.location = result;
+  USER_OBJECT.location = result;
   return new Promise((resolve) => {
     setTimeout(() => resolve('admin'), 4000);
   });
 }).then((result) => {
-  User.role = result;
-  User.mail = `semenkovvlad@${result}.com`
+  USER_OBJECT.role = result;
+  USER_OBJECT.mail = `semenkovvlad@${result}.com`
   return new Promise((resolve) => {
     setTimeout(() => resolve('1'), 2000);
   });
 }).then((result) => {
-  User.id = result;
+  USER_OBJECT.id = result;
   return new Promise((resolve) => {
     setTimeout(() => resolve(['Саша', 'Влад', 'Юля', 'Андрей', 'Богдан']), 1000);
   });
 }).then((result) => {
-  if(User.role === 'admin'){
-    if(ID[User.id] > result.length) {
+  if(USER_OBJECT.role === 'admin'){
+    if(OBJECT_OF_ID[USER_OBJECT.id] > result.length) {
       throw new Error('Count of people too much!');
     }
   function addFriends(id) {
-    User.friends = [];
-    for(let i = 0;i < ID[User.id];i++){
-      User.friends.push(result[i]);
+    USER_OBJECT.friends = [];
+    for(let i = 0;i < OBJECT_OF_ID[User.id];i++){
+      USER_OBJECT.friends.push(result[i]);
     }
   }
-  addFriends(ID[User.id]);
+  addFriends(ID[USER_OBJECT.id]);
 } else throw new Error('You are not admin!');
 }).catch(error => console.log(error));
--- task3AndersenPromise2.js
+++ task3AndersenPromise2.js
@@ -7,18 +7,17 @@ function request(url) {
 }
 
 function resolveUrlsArray(urls) {
-  let ArrayOfUrls = urls.map(function (url) {
-      return request(url);
+  const arrayOfUrls = urls.map{(url) => request(url);
   });
   const resultArray = [];
     return new Promise((resolve, reject) => {
-      for (let i = 0; i < ArrayOfUrls.length; i++) {
-        ArrayOfUrls[i]
-            .then(res => {
+      for (let i = 0; i < arrayOfUrls.length; i++) {
+        arrayOfUrls[i]
+            .then((res) => {
               resultArray.push(res);
-              if (resultArray.length === ArrayOfUrls.length) resolve(resultArray);
+              if (resultArray.length === arrayOfUrls.length) resolve(resultArray);
             })
-            .catch(error => {
+            .catch((error) => {
               reject(error);
             });
       }
