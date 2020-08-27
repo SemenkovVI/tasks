ea2885fc79213d5fea2a8d1db0f48db0a38501d7
Vlad Semenkov <56255056+SemenkovVI@users.noreply.github.com>
2020-08-27 15:07:05 +0300

    fixed codestyle

--- task3AndersenPromise2.js
+++ task3AndersenPromise2.js
@@ -1,26 +1,24 @@
 function request(url) {
   return new Promise((res) => {
-      const delayTime = Math.floor(Math.random() * 10000) + 1;
-      setTimeout(() => res(url), delayTime);
+    const delayTime = Math.floor(Math.random() * 10000) + 1;
+    setTimeout(() => res(url), delayTime);
   });
 
 }
 
 function resolveUrlsArray(urls) {
-  let ArrayOfUrls = urls.map(function (url) {
-      return request(url);
-  });
+  const arrayOfUrls = urls.map((url) => request(url));
   const resultArray = [];
-    return new Promise((resolve, reject) => {
-      for (let i = 0; i < ArrayOfUrls.length; i++) {
-        ArrayOfUrls[i]
-            .then(res => {
-              resultArray.push(res);
-              if (resultArray.length === ArrayOfUrls.length) resolve(resultArray);
-            })
-            .catch(error => {
-              reject(error);
-            });
-      }
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < arrayOfUrls.length; i++) {
+      arrayOfUrls[i]
+        .then((res) => {
+          resultArray.push(res);
+          if (resultArray.length === arrayOfUrls.length) resolve(resultArray);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    }
   });
 }
