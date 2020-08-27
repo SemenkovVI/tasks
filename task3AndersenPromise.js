12cda0e65485281fb850c83529caef98ef3b916a
Vlad Semenkov <56255056+SemenkovVI@users.noreply.github.com>
2020-08-27 15:00:18 +0300

    fixed codestyle moments

---
+++ task3AndersenPromise.js
@@ -0,0 +1,39 @@
+const USER_OBJECT = {};
+const OBJECT_OF_ID = {
+  1: 7,
+  2: 5,
+  3: 3,
+};
+
+const promise1 = new Promise((resolve) => {
+  setTimeout(() => resolve('Minsk'), 1000); 
+}).then((result) => {
+  USER_OBJECT.location = result;
+  return new Promise((resolve) => {
+    setTimeout(() => resolve('admin'), 4000);
+  });
+}).then((result) => {
+  USER_OBJECT.role = result;
+  USER_OBJECT.mail = `semenkovvlad@${result}.com`;
+  return new Promise((resolve) => {
+    setTimeout(() => resolve('1'), 2000);
+  });
+}).then((result) => {
+  USER_OBJECT.id = result;
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(['Саша', 'Влад', 'Юля', 'Андрей', 'Богдан']), 1000);
+  });
+}).then((result) => {
+  if(USER_OBJECT.role === 'admin'){
+    if(OBJECT_OF_ID[USER_OBJECT.id] > result.length) {
+      throw new Error('Count of people too much!');
+    }
+  function addFriends(id) {
+    USER_OBJECT.friends = [];
+    for(let i = 0;i < OBJECT_OF_ID[User.id];i++){
+      USER_OBJECT.friends.push(result[i]);
+    }
+  }
+  addFriends(ID[USER_OBJECT.id]);
+} else throw new Error('You are not admin!');
+}).catch(error => console.log(error));
---
+++ task3AndersenPromise2.js
@@ -0,0 +1,24 @@
+function request(url) {
+  return new Promise((res) => {
+      const delayTime = Math.floor(Math.random() * 10000) + 1;
+      setTimeout(() => res(url), delayTime);
+  });
+
+}
+
+function resolveUrlsArray(urls) {
+  const arrayOfUrls = urls.map((url) => request(url));
+  const resultArray = [];
+    return new Promise((resolve, reject) => {
+      for (let i = 0; i < arrayOfUrls.length; i++) {
+        arrayOfUrls[i]
+            .then((res) => {
+              resultArray.push(res);
+              if (resultArray.length === arrayOfUrls.length) resolve(resultArray);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+      }
+  });
+}
