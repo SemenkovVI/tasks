8ca389c8e0eb6ca73ed7bc52e495d4d608d54f39
влад семенков <semenkovvlad2000@gmail.com>
2020-09-01 13:56:18 +0000

    task3AndersenPromise2.js edited online with Bitbucket

--- task3AndersenPromise2.js
+++ task3AndersenPromise2.js
@@ -1,7 +1,7 @@
 function request(url) {
   return new Promise((res) => {
-    const delayTime = Math.floor(Math.random() * 10000) + 1;
-    setTimeout(() => res(url), delayTime);
+      const delayTime = Math.floor(Math.random() * 10000) + 1;
+      setTimeout(() => res(url), delayTime);
   });
 
 }
@@ -9,16 +9,16 @@ function request(url) {
 function resolveUrlsArray(urls) {
   const arrayOfUrls = urls.map((url) => request(url));
   const resultArray = [];
-  return new Promise((resolve, reject) => {
-    for (let i = 0; i < arrayOfUrls.length; i++) {
-      arrayOfUrls[i]
-        .then((res) => {
-          resultArray.push(res);
-          if (resultArray.length === arrayOfUrls.length) resolve(resultArray);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    }
+    return new Promise((resolve, reject) => {
+      for (let i = 0; i < arrayOfUrls.length; i++) {
+        arrayOfUrls[i]
+            .then((res) => {
+              resultArray.push(res);
+              if (resultArray.length === arrayOfUrls.length) resolve(resultArray);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+      }
   });
 }
