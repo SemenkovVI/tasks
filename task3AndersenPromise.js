ba9c07d9b516bd2d14eb74b3a3b0daff8742addc
влад семенков <semenkovvlad2000@gmail.com>
2020-09-01 13:58:09 +0000

    task3AndersenPromise.js edited online with Bitbucket

--- task3AndersenPromise.js
+++ task3AndersenPromise.js
@@ -6,7 +6,7 @@ const OBJECT_OF_ID = {
 };
 
 const promise1 = new Promise((resolve) => {
-  setTimeout(() => resolve('Minsk'), 1000); 
+  setTimeout(() => resolve('Minsk'), 1000);
 }).then((result) => {
   USER_OBJECT.location = result;
   return new Promise((resolve) => {
@@ -24,16 +24,17 @@ const promise1 = new Promise((resolve) => {
     setTimeout(() => resolve(['Саша', 'Влад', 'Юля', 'Андрей', 'Богдан']), 1000);
   });
 }).then((result) => {
-  if(USER_OBJECT.role === 'admin') {
-    if(OBJECT_OF_ID[USER_OBJECT.id] > result.length) {
+  if (USER_OBJECT.role === 'admin') {
+    if (OBJECT_OF_ID[USER_OBJECT.id] > result.length) {
       throw new Error('Count of people too much!');
     }
-  function addFriends(id) {
-    USER_OBJECT.friends = [];
-    for(let i = 0;i < OBJECT_OF_ID[User.id];i++){
-      USER_OBJECT.friends.push(result[i]);
+
+    function addFriends(id) {
+      USER_OBJECT.friends = [];
+      for (let i = 0; i < OBJECT_OF_ID[User.id]; i++) {
+        USER_OBJECT.friends.push(result[i]);
+      }
     }
-  }
-  addFriends(ID[USER_OBJECT.id]);
-} else throw new Error('You are not admin!');
+    addFriends(ID[USER_OBJECT.id]);
+  } else throw new Error('You are not admin!');
 }).catch(error => console.log(error));
